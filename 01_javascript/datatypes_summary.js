e8ac68dd58fe30fa33192b0aa8e3c2a3e115457f
aryanjadhav <aryanjadhav964@gmail.com>
2025-11-20 17:17:15 +0000

    adding comments for referance

--- 01_javascript/datatypes_summary.js
+++ 01_javascript/datatypes_summary.js
@@ -3,32 +3,32 @@
 //types : 7 => string, number, boolean, null, undefined, symbol, bigint
 
 
-let num = 100
-let newNum = 100.30
+let num = 100 //dataType number
+let newNum = 100.30 //dataType number
 
-const isLoggedIn = true
-const outSideTemp = null
+const isLoggedIn = true //dataType boolean
+const outSideTemp = null //dataType object
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol('123')  //dataType object
+const anotherId = Symbol('123') //dataType object
 
-const bigNum = 1234567n
+const bigNum = 1234567n //dataType bigInt
 // console.log(id === anotherId);
 
 
 //Referance (Non primitive) => Array, Objects, Functions
 
-let arr = ["Aryan", "Viraj", "Raj", "Nikhil"];
+let arr = ["Aryan", "Viraj", "Raj", "Nikhil"]; //dataType object
 
 const student = {
     name : "Aryan",
     age : 18,
     isPass : true,
     score : 80.61
-}
+} //dataType object
 
 const myFunction = function () {
     console.log("Hello World!");
-}
+}  //dataType Function
 
-console.log(typeof arr);
+console.log(typeof newNum);
